edfbcc6cfbe543d89c56da41915d5e02c3e36090
huangzx <huangzhixi@youxin.cloud>
2021-10-20 09:33:32 +0800

    fixed

--- src/typeof.js
+++ src/typeof.js
@@ -6,10 +6,4 @@
 const check = (target) =>
   Object.prototype.toString.call(target).slice(8, -1).toLowerCase();
 
-const set = new Set();
-set.add('ConardLi');
-set.add('code秘密花园');
-
-console.log(check(set), '===');
-
 module.exports = check;
