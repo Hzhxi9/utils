0c3badffd189dcd5bd305a4b651722239ffe23e9
huangzx <huangzhixi@youxin.cloud>
2021-10-22 10:00:26 +0800

    forin

--- src/forEach.js
+++ src/forEach.js
@@ -1,3 +1,5 @@
+const { isObject, isUndefined } = require('./typeof');
+
 /**
  * forin 遍历效率低, while 遍历最高
  * 用 while 实现 forin 遍历
@@ -6,11 +8,37 @@
  * @returns
  */
 
-module.exports = function forEach(array, iterator) {
+function forEach(array, iterator) {
   let idx = -1;
   const len = array.length;
   while (++idx < len) {
     iterator(array[idx], idx, array);
   }
   return array;
+}
+
+function forin(target, fn) {
+  if (target === null || isUndefined(target)) return;
+
+  /**如果不为对象, 包装为对象 */
+  if (!isObject(target)) target = [target];
+
+  if (Array.isArray(target)) {
+    const len = target.length >>> 0;
+    let k = 0;
+    while (k < len) {
+      fn.call(null, target[k], k, target);
+      k++;
+    }
+  } else {
+    for (const key in target) {
+      if (Object.prototype.hasOwnProperty.call(target, key))
+        fn.call(null, target[key], key, target);
+    }
+  }
+}
+
+module.exports = {
+  forEach,
+  forin
 };
